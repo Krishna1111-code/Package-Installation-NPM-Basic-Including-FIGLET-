9b69c6cd01b93fe1dcb2febc3d9d2535eb308f60
CodeFox <ks4473310@gmail.com>
2025-09-14 16:11:50 +0530

    Initial Commit GitHub Push

---
+++ math.js
@@ -0,0 +1,19 @@
+export const g = 8.5
+export const PI = 3.14
+
+// IF REQUIRE THEN USE REQUIRE ONLY 
+// IF IMPORT THEN USE IMPORT ONLY 
+
+// NO MIX ❌
+
+
+// FIRST EXPORT THEN IMPORT 
+// EXPORT = KEYWORD 
+// INDIVISUAL EXPORT 
+// NO OBJECT EXPORT LIKE MODULE.EXPORT 
+
+
+// export
+// npm init create a package.json 
+// add in package.json       "type" : "module"
+// import {something} from "./file_name" 
---
+++ script.js
@@ -0,0 +1,22 @@
+// const figlet = require('figlet');
+
+// figlet("Hello World!!", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+// INSTALLING GLOABLLY 
+
+
+
+
+// IMPORT
+import {g , PI} from "./math.js"
+import { generate } from "random-words";   // Random Words 
+// console.log(PI)
+// console.log(g)
+console.log(generate());
